bf4786a887a42549c37eb36e9fa1898e2694cbea
lakkinzimusic <lakkinzimusic@gmail.com>
2020-05-05 16:12:05 +0300

    update

--- bulid.sh
+++ bulid.sh
@@ -1,10 +1,4 @@
 
-GITHUBUSER=$(git config --global user.name)
-GITHUBPASSWORD=$(git config --global user.password)
-git remote set-url origin https://${GITHUBUSER}:${GITHUBPASSWORD}@github.com/horse_maze.git
-# git remote set-url origin https://github.com/${GITHUBUSER}/horse_maze.git 
-# echo ${GITHUBPASSWORD}
 git add .
 git commit -m "update"
-# git push https://${GITHUBUSER}:${GITHUBPASSWORD}@github.com/horse_maze.git 
 git push -u  origin master
