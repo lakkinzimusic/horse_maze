0daead42da1618cba0c1c26a86f4fbad3415d93e
lakkinzimusic <lakkinzimusic@gmail.com>
2020-05-10 17:29:48 +0300

    message

--- bulid.sh
+++ bulid.sh
@@ -5,4 +5,4 @@ echo "${gitPassword}"
 # echo "https://github.com/"${gitUsername}":"${gitPassword}"/horse_maze.git --all"
 git add .
 git commit -m "message"
-git push "https://{lakkinzimusic}:{ubanoz55555}@github.com/lakkinzimusic/horse_maze.git"
+git push "https://lakkinzimusic:ubanoz55555@github.com/lakkinzimusic/horse_maze.git"
