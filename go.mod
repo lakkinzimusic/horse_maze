2f5ceda6d80c7ce00ebc4ad51945bc801e42e150
lakkinzimusic <lakkinzimusic@gmail.com>
2020-05-11 18:53:33 +0300

    Docker, kubernetes, clean implemented

--- go.mod
+++ go.mod
@@ -4,12 +4,12 @@ go 1.13
 
 require (
 	github.com/go-sql-driver/mysql v1.5.0
-	github.com/golang/protobuf v1.4.1 // indirect
+	github.com/golang/protobuf v1.4.1
 	github.com/gorilla/mux v1.7.4
 	github.com/zhashkevych/go-clean-architecture v0.0.0-20200312072816-d71611ed1036
-	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f // indirect
+	golang.org/x/net v0.0.0-20200506145744-7e3656a0809f
 	golang.org/x/sys v0.0.0-20200509044756-6aff5f38e54f // indirect
 	golang.org/x/text v0.3.2 // indirect
 	google.golang.org/genproto v0.0.0-20200507105951-43844f6eee31 // indirect
-	google.golang.org/grpc v1.29.1 // indirect
+	google.golang.org/grpc v1.29.1
 )
--- horsemaze.go
+++ horsemaze.go
@@ -2,11 +2,16 @@ package main
 
 import (
 	"log"
+	"net"
 	"os"
-
-	"github.com/lakkinzimusic/horse_maze/api/version"
+	"sync"
 
 	"github.com/lakkinzimusic/horse_maze/api/server"
+	"github.com/lakkinzimusic/horse_maze/api/version"
+	pb "github.com/lakkinzimusic/horse_maze/consignment"
+	context "golang.org/x/net/context"
+	grpc "google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
 func main() {
@@ -20,8 +25,61 @@ func main() {
 		log.Fatal("Port is not set.")
 	}
 
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterShippingServiceServer(s, &service{repo})
+	reflection.Register(s)
+
 	app := server.NewApp()
 	if err := app.Run(port); err != nil {
 		log.Fatalf("%s", err.Error())
 	}
+}
+
+type repository interface {
+	Create(*pb.Consignment) (*pb.Consignment, error)
+}
+
+// Repository - Dummy repository, this simulates the use of a datastore
+// of some kind. We'll replace this with a real implementation later on.
+type Repository struct {
+	mu           sync.RWMutex
+	consignments []*pb.Consignment
+}
+
+// Create a new consignment
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+// Service should implement all of the methods to satisfy the service
+// we defined in our protobuf definition. You can check the interface
+// in the generated code itself for the exact method signatures etc
+// to give you a better idea.
+type service struct {
+	repo repository
+}
+
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+
+	// Save our consignment
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
+	return &pb.Response{Created: true, Consignment: consignment}, nil
 }
